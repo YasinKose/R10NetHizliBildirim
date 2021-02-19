bc21706573b012d08478c0822eb0757750f39096
YasinKose <yasin.kose.123@gmail.com>
2021-02-19 22:09:33 +0300

    İlk Commit

---
+++ action.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	$("body").append("<script>setInterval('$.bildirimKontrol()',(1000))</script>");
+});
---
+++ background.js
@@ -0,0 +1,12 @@
+﻿// This is the background page.
+// it keeps track of prefrences/settings in localStorage
+
+if (typeof chrome !== "undefined") {
+  var browser = chrome;
+}
+
+browser.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "https://www.r10.net";
+    browser.tabs.create({ url: newURL });
+});
